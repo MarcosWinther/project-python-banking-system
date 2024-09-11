1b0bc27ad3f0761b7fb091f838475e3f872f59cf
MarcosWinther <winthermarcos@gmail.com>
2024-09-11 00:10:24 -0300

    :recycle: refactor: Transformado as ações de sacar e depositar em funções e criando duas novas funções: criar usuário e criar conta, mas essas últimas funções ainda estão sendo implementadas

--- desafio_sistema_bancario.py
+++ desafio_sistema_bancario.py
@@ -1,10 +1,73 @@
-def exibir_saldo_conta(saldo):
-     print("============= EXTRATO =============")
-     return print(f"O saldo da conta: R${saldo:.2f}")
+def criar_usuario(nome, data_nasc, cpf, endereco):
+    print("Criar usuário")
 
-#--------------------------------------
+def criar_conta_corrente():
+    print("Criar conta corrente")
 
-menu = """
+# Função para depósito
+def depositar(saldo, valor, extrato):
+    
+    if(valor > 0):
+        saldo += valor
+        extrato = f"Valor depositado: R${saldo:.2f}"
+        return saldo, print(extrato)
+    
+    extrato = "Depósito não realizado! Verifique o valor e tente novamente"
+
+    return saldo, print(extrato)
+
+# Funções para saque
+def verificar_limite_saque(numero_saque, limite_qtd_saque):
+    
+    if(numero_saque >= limite_qtd_saque):
+        return True
+    
+    return False
+
+def verificar_valor_limite(valor_saque, valor_limite):
+
+    if valor_saque >= valor_limite:
+        return True
+    
+    return False
+
+def sacar(*, saldo, valor, qtd_saque, limite_qtd_saque, limite_saque, extrato):
+
+    quantidade_saque_excedeu = verificar_limite_saque(qtd_saque, limite_qtd_saque)
+    extrato = "Transação não realizada"
+
+    if quantidade_saque_excedeu:
+        extrato = "Você ultrapassou a quantidade de saques disponíveis!"
+
+    elif valor > 0 and valor <= saldo:
+        valor_limite_excedeu = verificar_valor_limite(valor, limite_saque)
+
+        if valor_limite_excedeu:
+            extrato = "Você ultrapassou o valor do limite permitido para saque!"
+
+        else:
+            saldo -= valor
+            qtd_saque += 1
+            print(qtd_saque)
+            extrato = f"Saque realizado com sucesso! Valor do saque R${valor:.2f}\nSaldo atual: R${saldo:.2f}"
+
+    elif valor > saldo or saldo == 0:
+        extrato = "Saldo insufiente para essa transação"
+
+    else:
+        extrato = "Valor inválido! Verifique o valor e tente novamente"
+    
+    return qtd_saque, saldo, print(extrato)
+
+# Função para extrato    
+def exibir_extrato_bancario(extrato, /, *, saldo):
+    print("============= EXTRATO =============")
+    extrato = f"O saldo da conta: R${saldo:.2f}"
+    return saldo, print(extrato)
+
+#----------------------------------------------------------------------------
+
+menu = '''
 DIGITE A OPÇÃO DESEJADA:
 
 [d] - Depositar
@@ -12,10 +75,11 @@ DIGITE A OPÇÃO DESEJADA:
 [e] - Extrato Bancário
 [q] - Sair
 
-=> """
+=> '''
 
 saldo_conta = 0
 valor_limite_saque = 500
+extrato = ''
 numero_saques = 0
 LIMITES_SAQUES = 3
 
@@ -23,48 +87,30 @@ while True:
     
     opcao = input(menu)
 
-    if opcao == "d":
-        valor_deposito = float(input("Informe o valor do depósito: R$"))
-        
-        if valor_deposito > 0:
-             saldo_conta += valor_deposito
-             print("Depósito realizado com sucesso!")
-             exibir_saldo_conta(saldo=saldo_conta)
+    if opcao == 'd':
 
-        else:
-            print("Depósito não realizado! verifique o valor e tente novamente!")
+        valor_deposito = float(input('Informe o valor do depósito: R$'))
+
+        saldo_conta, extrato = depositar(saldo_conta, valor_deposito, extrato)
             
-    elif opcao == "s":
+    elif opcao == 's':
         
-        valor_saque = float(input("Informe o valor do saque: R$"))
+        valor_saque = float(input('Informe o valor do saque: R$'))
 
-        excedeu_qtd_saques = numero_saques >= LIMITES_SAQUES
-
-        excedeu_valor_limite = valor_saque >= valor_limite_saque
-
-        if excedeu_qtd_saques:
-            print("Transação não realizada! Você ultrapassou a quantidade de saques disponíveis!")
+        numero_saques, saldo_conta, extrato = sacar(
+            saldo=saldo_conta, 
+            valor=valor_saque, 
+            qtd_saque=numero_saques, 
+            limite_qtd_saque=LIMITES_SAQUES, 
+            limite_saque=valor_limite_saque, 
+            extrato=extrato
+        )
         
-        elif (valor_saque > 0) and (valor_saque <= saldo_conta):
-            if excedeu_valor_limite:
-                print("Transação não realizada! Você ultrapassou o valor do limite permitido para saque!")
+    elif opcao == 'e':
+        extrato, saldo_conta = exibir_extrato_bancario(extrato, saldo=saldo_conta)
 
-            else:
-                numero_saques += 1
-                saldo_conta -= valor_saque
-                exibir_saldo_conta(saldo=saldo_conta)
-        
-        elif saldo_conta == 0:
-            print("Saldo insuficiente para essa transação!")
-
-        else:
-            print("Valor inválido! Verifique o valor e tente novamente!")
-        
-    elif opcao == "e":
-        exibir_saldo_conta(saldo=saldo_conta)
-
-    elif opcao == "q":
+    elif opcao == 'q':
         break
-
+    
     else:
-        print("Operação inválida! Por favor selecionar novamente a operação desejada com a opção correta!")
+        print("Opcão inválida! Tente novamente!")
